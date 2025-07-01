e89c9e81f15aadde399aff4fbd78a66eedb63698
Kin-Doung <kin.doung@student.passerellesnumeriques.org>
2025-07-01 15:45:21 +0700

    create table

---
+++ back-end/app/Http/Controllers/ProductReviewController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product_Review;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductReviewController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product_Review $product_Review)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product_Review $product_Review)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product_Review $product_Review)
+    {
+        //
+    }
+}
